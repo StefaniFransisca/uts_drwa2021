4b8ca186c5bb47f675b84d242c26f82a0aa48be2
stefanifransisca <stefanifransisca88@gmail.com>
2022-03-29 19:50:52 +0700

    uts

---
+++ guru/guru/Models/GuruItem.cs
@@ -0,0 +1,16 @@
+﻿namespace guru.Model
+{
+    public class GuruItem
+    {
+        private GuruContext context;
+        public int id_guru { get; set; }
+        public string rfid { get; set; }
+        public string nip { get; set; }
+        public string nama_guru { get; set; }
+        public string alamat { get; set; }
+        public int status { get; set; }
+        public int id_mapel { get; set; }
+        public string nama_mapel { get; set; }
+        public string deskripsi { get; set; }
+    }
+}
